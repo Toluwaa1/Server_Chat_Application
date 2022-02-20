680f761ed23a63590b31d832ba250f453e8644a7
Adewu <Adewu@TABLET-M9S88RL3>
2022-02-20 02:52:50 +0100

    Add project files.

---
+++ Adewu/Model/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adewuyi_Alawusa_chat.Adewu.Model
+{
+    class UserModel
+    {
+        public string UserName { get; set; }
+        public string UID { get; set; }    
+    }
+
+}
